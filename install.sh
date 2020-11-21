d572a1e739c5643b478b67cbd371d87fdac5a772
shoowl <71995271+shoowl@users.noreply.github.com>
2020-11-21 20:35:02 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+clear 
+
+pkg install unstable-repo -y
+
+pkg install metasploit -y
+
+rm -rf install.sh
+
+clear
+
+cowsay -f eyes "Hacking" | lolcat
+figlet -f slant "FERRAMENTAS" | lolcat
+
+    echo "Execulte o backdoortst.sh"
+
+
